6ede53834f0506c7b49243d7f3be29869c3c0ed9
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-04-21 15:34:28 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,6 @@
-
-
+const getUsersWithFriend = (users, friendName) => {
+    return users.filter(user => user.friends.includes(friendName));
+};
 
 const allUsers = [
     {
@@ -31,8 +32,9 @@ const allUsers = [
       friends: ["Goldie Gentry", "Briana Decker"]
     }
   ];
-  
-  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
   // [
   //   {
   //     name: "Sharlene Bush",
